f217ba7a3209410c55ef72987cc25b155b8f1849
Ayoung <saing.sab@gmail.com>
2023-09-15 10:55:54 +0700

    cars assemble

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,22 @@
 (ns cars-assemble)
 
+(def default-rate 221)
+
+(defn cal-rate [speed]
+  (cond (= speed 10) 0.77
+        (= speed 9) 0.8
+        (>= speed 5) 0.9
+        :else 1.0))
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  )
+  (* (* default-rate speed)
+     (cal-rate speed)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
-  )
+  (let [pro-rate (production-rate speed)]
+    (int (/ pro-rate 60))))
