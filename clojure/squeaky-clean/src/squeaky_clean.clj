c614ed8b220983a385aeda3422d34a25e61d6b4b
Ayoung <saing.sab@gmail.com>
2023-09-18 18:19:46 +0700

    squeaky-clean

--- clojure/squeaky-clean/src/squeaky_clean.clj
+++ clojure/squeaky-clean/src/squeaky_clean.clj
@@ -1,10 +1,32 @@
 (ns squeaky-clean
   (:require [clojure.string :as str]))
 
+(defn- rm-space 
+  [s]
+  (str/replace s " " "_"))
+
+(defn- rp-ctrl-chars 
+  [s]
+  (str/replace s #"[\u0000-\u001F\u007F-\u009F]" "CTRL"))
+
+(defn- to-camel-case
+  [s]
+  (str/replace s #"-(\p{L})" #(str/upper-case (%1 1))))
+
+(defn- om-non-letter-chars
+  [s]
+  (str/replace s #"[^\p{L}_]" ""))
+
+(defn- om-greek-lower-letters
+  [s]
+  (str/replace s #"[\u03B1-\u03C9]" ""))
+
 (defn clean
   "TODO: add docstring"
   [s]
-  (str/replace s " " "_")
-  (if (str/includes? '\u0000' '\u001F' '\u007F' '\u009F') (str/replace s " " "CTRL"))
-  ;; (str/replace s "CTRL" "") 
-  )
+  (-> s 
+      (rm-space)
+      (rp-ctrl-chars)
+      (to-camel-case)
+      (om-non-letter-chars)
+      (om-greek-lower-letters)))
