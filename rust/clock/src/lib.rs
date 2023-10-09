f865968903e5a6876b68efc40698f6b0d4875fa8
Ayoung <saing.sab@gmail.com>
2023-10-09 09:54:28 +0700

    solved

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,17 +1,27 @@
+use std::fmt;
+const MINUTES_PER_DAY: i64 = 24*60;
+const MINUTES_PER_HOUR: i64 = 60;
+#[derive(Debug, Eq, PartialEq)]
 pub struct Clock {
-    hours: i32,
-    minutes: i32,
+    minutes: i64,
 }
-
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        /*  todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
-         hours.to_string(); */
-        String::from(hours.to_string());
-
+    pub fn new(hours: i64, minutes: i64) -> Self {        
+        Clock {
+            minutes: (hours * MINUTES_PER_HOUR + minutes).rem_euclid(MINUTES_PER_DAY),
+        }
     }
-
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+    pub fn add_minutes(&self, minutes: i64) -> Self {
+        Clock::new(0, self.minutes + minutes)
+    }
+}
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.minutes.div_euclid(MINUTES_PER_HOUR),
+            self.minutes.rem_euclid(MINUTES_PER_HOUR)
+        )
     }
 }
