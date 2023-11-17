26f6d6af03bb7edb174cf4d2736b664fb3a64ed8
Ayoung <saing.sab@gmail.com>
2023-11-17 10:00:40 +0700

    solved anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,19 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+// normalize charactors by alphabet a->z
+fn normalize(s: &str) -> String {
+    let mut s_vec = s.to_lowercase().chars().collect::<Vec<char>>();
+    s_vec.sort_unstable();
+    
+    s_vec.iter().collect()
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let n_word = normalize(word).to_lowercase();
+    possible_anagrams
+        .iter()
+        .filter(|s| s.to_lowercase() != word.to_lowercase() && normalize(s) == n_word)
+        .copied()
+        .collect()
+
 }
